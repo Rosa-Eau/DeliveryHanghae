2e3b46c2d50149295a0d4de4aa2f59c2654c9622
weiß Yi <78130010+rosa-eau@users.noreply.github.com>
2024-03-12 22:22:15 +0900

    style: architecture create

---
+++ src/main/java/com/example/demo/domain/common/signup/dto/LoginRequestDto.java
@@ -0,0 +1,10 @@
+package com.example.demo.domain.common.signup.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class LoginRequestDto {
+
+}
---
+++ src/main/java/com/example/demo/domain/common/signup/repository/MemberRepository.java
@@ -0,0 +1,12 @@
+package com.example.demo.domain.common.signup.repository;
+
+import com.example.demo.domain.common.signup.entity.Member;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface MemberRepository extends JpaRepository<Member, Long> {
+    boolean existsByEmail(String email);
+
+    Optional<Member> findByEmail(String email);
+}
